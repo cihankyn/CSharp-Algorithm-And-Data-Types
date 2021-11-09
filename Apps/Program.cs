02bc613bca6911488bacfeed477f5133addc3673
Cihan Koyun <chnkyn01@gmail.com>
2021-11-09 04:50:20 +0300

    First schema was created.

---
+++ Apps/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Apps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
